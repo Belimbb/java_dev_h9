108ad428428a5fde0b7c33843068ec17bfdc42d2
Belimbb <belovmladshui@gmail.com>
2024-03-09 16:51:34 +0200

    hotfix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,18 +4,14 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args){
- /*
         HttpStatusChecker httpStatusChecker = new HttpStatusChecker(226);
         String imageUrl = httpStatusChecker.getImageUrl();
         System.out.println("imageUrl = " + imageUrl);
 
         HttpStatusImageDownloader httpSID = new HttpStatusImageDownloader();
         httpSID.downloadStatusImage(435467);
- */
 
         HttpImageStatusCli httpISCli = new HttpImageStatusCli();
         httpISCli.askStatus();
-
-
     }
 }
